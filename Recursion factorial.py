ebe48ee9374417814a4bc8cd39e6f2f276f5171e
Farhazni Najwa <141621627+hazniy@users.noreply.github.com>
2024-09-30 15:47:16 +0100

    Update Recursion factorial.py

--- Recursion factorial.py
+++ Recursion factorial.py
@@ -20,3 +20,31 @@ while i <=10: #(n =10)
     factorial = factorial * i 
     i = i + 1 
 print("10! =", factorial) 
+
+#first homework MAS 117
+#3
+x = 13
+y = 21
+x = y + 10
+y = x + 10 
+print("x = ", x, "and y = ", y)
+
+x = 13 
+y = 21
+x, y = y +10, x+10 
+print("x = ", x, "and y = ", y)
+
+#4 triangular number program (recursion)
+n = int(input("enter a number: "))
+triangular = 0
+i = 1
+
+if n == 0: 
+    print("maths error bruh")
+else: 
+    while i <= n: 
+        formula = 1/(i**2)
+        triangular = triangular + formula
+        i = i + 1
+    print(triangular) 
+   
