076d5b9c733ea5a98ba46a8f9d1e4850970ca2c3
Farhazni Najwa <141621627+hazniy@users.noreply.github.com>
2024-09-30 11:47:02 +0100

    1st Lecture

--- Recursion factorial.py
+++ Recursion factorial.py
@@ -1,3 +1,4 @@
+#create a function
 def factorial(number):
     if number == 0:
         return 1
@@ -11,3 +12,11 @@ if num < 0:
    print("Sorry, factorial does not exist for negative numbers")
 else:
    print("The factorial of", num, "is", factorial(num))
+
+#basic factorial (example 10) 
+factorial = 1 
+i = 1 
+while i <=10: #(n =10)
+    factorial = factorial * i 
+    i = i + 1 
+print("10! =", factorial) 
